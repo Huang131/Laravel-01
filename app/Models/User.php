960f64b23fc7894f9ca49665f3b120479e38cff4
huang <1315970725@qq.com>
2019-04-09 19:16:20 +0800

    关注用户动态

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
+
 
 class User extends Authenticatable
 {
@@ -61,10 +63,14 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }  
 
+
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 
 
